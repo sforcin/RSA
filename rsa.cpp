1b1fa7a85fb1ec56ff6da670c62e5ed637433872
isabela sforin <sforcin@isabelas-MacBook-Pro.local>
2024-10-29 14:29:06 -0700

    checking if both numbers are primes

--- rsa.cpp
+++ rsa.cpp
@@ -7,39 +7,55 @@ using namespace std;
 
 void getMsg(vector<int> &encoded, int M);
 bool isPrime(int num);
+bool isTwoPrimes(int n);
 
 int main() {
     // Ask user for e, n, and m, where m is the amount of numbers to decrypt, then ask for the numbers to decrypt
     int e, n, m, p = 0, q = 0, phi;
     cout << "Enter e: ";
     cin >> e;
+    if(e < 1){
+        cout << "e is not valid!" << endl;
+        return 0;
+    }
     cout << "Enter n: ";
     cin >> n;
+
+    if (!isTwoPrimes(n)) {
+        cout << "n is not a product of two distinct primes" << endl;
+        return 0;
+    }
+
     cout << "Enter m: ";
     cin >> m;
 
     vector<int> encoded;
     getMsg(encoded, m);
 
-    bool found = false;
-    for (int i = 2; i <= sqrt(n) && !found; ++i) { // Start from 2 to avoid 0 and 1
-        if (n % i == 0) { // i is a factor of n
-            int j = n / i;
-            if (isPrime(i) && isPrime(j) && i != j) { // Check if both i and j are primes
-                p = i;
-                q = j;
-                found = true;
-                break;
+   bool found = false; 
+   for(int i = 0; i <= n && found != true; ++i){
+        for(int j = i + 1; j <= n; ++j){
+            //make sure they are distinct
+            if(i * j == n && i != j){
+                //make sure they are both primes
+                if(isPrime(i) == true && isPrime(j) == true){
+                    p = i; 
+                    q = j; //p < q(j) -> bigger val
+                    found = true; 
+                    break; 
+                }
             }
         }
-    }
-
+   }
     if (found) {
         cout << "p: " << p << endl;
         cout << "q: " << q << endl;
     } else {
         cout << "Failed to find distinct prime factors for n." << endl;
     }
+
+        phi = (p-1) * (q-1); 
+        cout<<"phi: "<<phi<<endl;
 
     return 0;
 }
@@ -59,7 +75,7 @@ void getMsg(vector<int> &encoded, int M) {
 
 bool isPrime(int num) {
     if (num <= 1) {
-        return false;
+        return false; // 0 and 1 are not prime
     }
     if (num == 2 || num == 3) {
         return true;
@@ -74,4 +90,18 @@ bool isPrime(int num) {
         }
     }
     return true;
+}
+
+bool isTwoPrimes(int n) {
+    if (n < 4) return false;  
+    
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {  // if i is a factor
+            int j = n / i;  // divide to get j, then check if j and i are primes and not the same
+            if (isPrime(i) && isPrime(j) && i != j) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
